67bf299bd215c5a4c8cc67599a116f3d6f7a5da5
dulara <it21014840@my.sliit.lk>
2023-01-06 08:56:10 +0530

    last commit

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,5 @@
+import random
+
 board = ["-", "-", "-",
          "-", "-", "-",
          "-", "-", "-"]
@@ -27,8 +29,86 @@ def playerInput(board):
         print("Oops player is already in that position")
 
 
+# check for win or tie
+def checkHorizontle(board):
+    global winner
+    if board[0] == board[1] == board[2] and board[1] != "-":
+        winner = board[0]
+        return True
+    elif board[3] == board[4] == board[5] and board[3] != "-":
+        winner = board[3]
+        return True
+    elif board[6] == board[7] == board[8] and board[6] != "-":
+        winner = board[6]
+        return True
 
+
+def checkRow(board):
+    global winner
+    if board[0] == board[3] == board[6] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[1] == board[4] == board[7] and board[1] != "-":
+        winner = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != "-":
+        winner = board[2]
+        return True
+
+
+def checkDiag(board):
+    global winner
+    if board[0] == board[4] == board[8] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[2] == board[4] == board[6] and board[2] != "-":
+        winner = board[2]
+        return True
+
+
+def checkTie(board):
+    if "-" not in board:
+        printBoard(board)
+        print("It is a tie!")
+        gameRunning = False
+
+
+def checkWin():
+    if checkDiag(board) or checkHorizontle(board) or checkRow(board):
+        print(f"The winner is {winner}")
+
+
+
+# switch the player
+
+def switchPlayer():
+    global currentPlayer
+    if currentPlayer == "X":
+        currentPlayer = "O"
+    else:
+        currentPlayer = "X"
+
+
+
+# computer
+
+def computer(board):
+    while currentPlayer == "O":
+        position = random.randint(0 , 8)
+        if board[position] == "-":
+            board[position] = "O"
+            switchPlayer()
+
+
+
+#check win or tie again 
 
 while gameRunning:
     printBoard(board)
     playerInput(board)
+    checkWin()
+    checkTie(board)
+    switchPlayer()
+    computer(board)
+    checkWin()
+    checkTie(board)
